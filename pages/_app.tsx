533fde933b9a8f6e87af18cd025dfc8c2bbb3ca8
HenricoW <henrico.ashley.White@gmail.com>
2022-04-08 10:16:04 +0200

    feat: landing page, mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { grey } from "@mui/material/colors";
+
+export const AppName = "DocuGree";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    text: {
+      primary: "#fff",
+      secondary: grey[500],
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
